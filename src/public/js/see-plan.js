18c725a39b75e48fde611a3e7e0333e293a7588c
nyoung113 <75849590+nyoung113@users.noreply.github.com>
2021-10-13 15:46:42 +0900

    날짜 별 지도, 위치 생성

--- src/public/js/see-plan.js
+++ src/public/js/see-plan.js
@@ -1,23 +1,11 @@
+const dayButtons = document.getElementsByClassName('btn');
+
+for(let i =0; i < dayButtons.length; i++){
+    
 
 
-const mapOptions = {
-    center : new naver.maps.LatLng(33.50088510909299, 126.52906251498592),
-    zoom : 10
-};
-
-const totMap = new naver.maps.Map(document.getElementById('totMap'), mapOptions);
-
-const marker = new naver.maps.Marker({
-    position : new naver.maps.LatLng(33.50088510909299, 126.52906251498592),
-    map: totMap
-});
-
-const marker2 = new naver.maps.Marker({
-    position : new naver.maps.LatLng(33.40002222233901, 126.55788921383366),
-    map: totMap
-});
-
-
+    
+}
 
 
 
