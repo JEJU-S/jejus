b2b19ac1e56e51f4fb67c53beeb3a8c583ead3d4
OhJeoung <90603596+OhJeoung@users.noreply.github.com>
2021-10-19 15:18:49 +0900

    toaster 기능 추가
    클릭 시 우측 여행 리스트에 추가하는 JS 추가

--- src/public/js/toastr.js
+++ src/public/js/toastr.js
@@ -2,10 +2,13 @@
 let removeToast;
 
 const result = document.getElementsByClassName("result");
-
 const toast = document.getElementById("toast");
 
 function handleOnclick1() {
+    var div = document.createElement('div');
+    div.innerHTML = document.getElementById('place-object').innerHTML;
+    document.getElementById('plcBox').appendChild(div);
+
 
     toast.classList.contains("reveal") ?
         (clearTimeout(removeToast), removeToast = setTimeout(function () {
@@ -13,7 +16,7 @@ function handleOnclick1() {
         }, 1000)) :
         removeToast = setTimeout(function () {
             document.getElementById("toast").classList.remove("reveal")
-        }, 1500)
+        }, 1000)
     toast.classList.add("reveal")
 
 }
@@ -21,4 +24,11 @@ function handleOnclick1() {
 for (var i = 0; i < result.length; i++) {
     var eachResult = result[i];
     eachResult.addEventListener('click', handleOnclick1);
+}
+
+const addBtn = document.getElementsByClassName("obj-add-btn");
+
+for (var i = 0; i < addBtn.length; i++) {
+    var eachaddBtn = addBtn[i];
+    eachaddBtn.addEventListener('click', handleOnclick1);
 }
