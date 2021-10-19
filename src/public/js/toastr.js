82cd1d61bdc162d10a80d6f731cafbc5113c8ca3
OhJeoung <90603596+OhJeoung@users.noreply.github.com>
2021-10-19 17:07:44 +0900

    edit plan 수정
    추천시스템 스크롤바 삽입을 위한 css 수정

--- src/public/js/toastr.js
+++ src/public/js/toastr.js
@@ -5,11 +5,12 @@ const result = document.getElementsByClassName("result");
 const toast = document.getElementById("toast");
 
 function handleOnclick1() {
+    //리스트에 추가하는 스크립트//
     var div = document.createElement('div');
     div.innerHTML = document.getElementById('place-object').innerHTML;
     document.getElementById('plcBox').appendChild(div);
 
-
+    //토스터 나왔다가 들어가는 스크립트//
     toast.classList.contains("reveal") ?
         (clearTimeout(removeToast), removeToast = setTimeout(function () {
             document.getElementById("toast").classList.remove("reveal")
@@ -20,7 +21,7 @@ function handleOnclick1() {
     toast.classList.add("reveal")
 
 }
-
+//클래스 하나하나에 숫자를 먹여 객체로 변환하고, 클릭 시 위의 함수가 실행되게 하는 스크립트//
 for (var i = 0; i < result.length; i++) {
     var eachResult = result[i];
     eachResult.addEventListener('click', handleOnclick1);
