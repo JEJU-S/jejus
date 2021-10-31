fd573038b1263cbf8463d148a28c1a72966a0c74
OhJeoung <90603596+OhJeoung@users.noreply.github.com>
2021-10-31 13:29:43 +0900

    디자인 수정 중

--- src/public/js/menu-opener.js
+++ src/public/js/menu-opener.js
@@ -1,13 +1,7 @@
 
-const opener = document.getElementById("group");
-
-function menuOpen(){
-    var con = document.getElementById('mygroup');
-    if (con.style.display == "flex"){ 
-            con.style.display = "none"; 
-    } else {
-        con.style.display = "flex"; 
-    }
-}
-
-opener.addEventListener('click', menuOpen);
+$(document).ready(function () {
+    $("#group").click(function (e) {
+        e.preventDefault();
+        $("#mygroup").slideToggle();
+    })
+})
