148b0f96809000c5c81ec939a51bb4ccf758be4f
OhJeoung <90603596+OhJeoung@users.noreply.github.com>
2021-11-18 17:34:46 +0900

    디자인작업

--- src/public/js/edit-plan/info.js
+++ src/public/js/edit-plan/info.js
@@ -1,17 +1,27 @@
 const info = document.getElementById("info")
-const infoBox = document.getElementsByClassName("dragdrop-info") 
+const infoBox = document.getElementsByClassName("dragdrop-info")
+const body = document.getElementById("main-content")
 
-function onOffInfo() {
+function onInfo() {
     if (infoBox[0].style.display == 'none'){
         infoBox[0].style.display='flex';
         infoBox[1].style.display='flex';
         infoBox[2].style.display='flex';
-    } else {
+    }else {
         infoBox[0].style.display='none';
         infoBox[1].style.display='none';
         infoBox[2].style.display='none';
     }
-    
 }
 
-info.addEventListener('click', onOffInfo);
+info.addEventListener('click', onInfo);
+
+function offInfo() {
+    if (infoBox[0].style.display == 'flex'){
+        infoBox[0].style.display='none';
+        infoBox[1].style.display='none';
+        infoBox[2].style.display='none';
+    }
+}
+
+body.addEventListener('click', offInfo);
